f9f75080ca6abc77063f2fbaed1e36170da441d2
Jean-François Ducarroz <noreply>
2014-03-18 19:40:42 +0000

    Add component qwe

---
+++ ui/qwe.reel/qwe.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/qwe.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Qwe
+ * @extends Component
+ */
+exports.Qwe = Component.specialize(/** @lends Qwe# */ {
+    constructor: {
+        value: function Qwe() {
+            this.super();
+        }
+    }
+});
