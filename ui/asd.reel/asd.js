b52cd9fdc9038da4fc699a489dcc24de2c3fa145
Jean-François Ducarroz <noreply>
2014-03-18 19:43:14 +0000

    Add component asd

---
+++ ui/asd.reel/asd.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/asd.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Asd
+ * @extends Component
+ */
+exports.Asd = Component.specialize(/** @lends Asd# */ {
+    constructor: {
+        value: function Asd() {
+            this.super();
+        }
+    }
+});
